14791afdbab14be6036040dbcf6928ec02aec882
sagar <s.sagar2468@gmail.com>
2023-10-01 18:17:13 +0530

    values and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,14 @@
 let js = "amazing";
-if (js === "amazing") alert("javascript is amazing");
+console.log(1 + 22 + 24);
+
+console.log("sagar");
+console.log(23);
+
+let firstName = "Sagar";
+console.log(firstName);
+
+let person = "sagar";
+let PI = 3.1415;
+
+let myFirstJob = "software engineer";
+console.log(myFirstJob);
