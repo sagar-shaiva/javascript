2c0ab58f24dd526144585d7260a812b274de7d61
sagar <s.sagar2468@gmail.com>
2023-10-02 11:22:23 +0530

    let const var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -16,22 +16,27 @@ let currentJob = "Product Support Engineer (Slack)";
 console.log(myFirstJob);
 */
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof true);
 // console.log(typeof 23);
 // console.log(typeof "sagar");
 
-javascriptIsFun = "yes!";
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "yes!";
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 2000;
-console.log(typeof year);
+// year = 2000;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// const birthYear = 2000;
+
+// var job = "teacher";
+// job = "coder";
