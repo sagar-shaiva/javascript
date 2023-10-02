bf7a03b85cc56ba0b8f65060a18dcff8bdbdb2fa
sagar <s.sagar2468@gmail.com>
2023-10-02 13:36:55 +0530

    challenge 1 complete

---
+++ challenges/script.js
@@ -0,0 +1,15 @@
+// Challenge
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMARK = massMark / (heightMark * heightMark);
+const BMIJOHN = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMARK);
+console.log(BMIJOHN);
+
+console.log(BMIMARK > BMIJOHN);
