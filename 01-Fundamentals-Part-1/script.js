b440596346c7064d63a44740ae1d993b7460da23
sagar <s.sagar2468@gmail.com>
2023-10-02 14:40:52 +0530

    string and template literals

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -41,31 +41,52 @@ console.log(myFirstJob);
 // var job = "teacher";
 // job = "coder";
 
-const now = 2023;
-const ageSagar = now - 2000;
-const ageMadhu = now - 2007;
-console.log(ageSagar);
-console.log(ageSagar * 2, ageMadhu / 2, 2 ** 3);
+// const now = 2023;
+// const ageSagar = now - 2000;
+// const ageMadhu = now - 2007;
+// console.log(ageSagar);
+// console.log(ageSagar * 2, ageMadhu / 2, 2 ** 3);
 
-const firstName = "S";
-const lastName = "Sagar";
-console.log(firstName + " " + lastName);
-let x = 10 + 5; //15
-x += 10; //x=x+10 = 25
-x *= 2;
-x++;
-x--;
-console.log(x);
+// const firstName = "S";
+// const lastName = "Sagar";
+// console.log(firstName + " " + lastName);
+// let x = 10 + 5; //15
+// x += 10; //x=x+10 = 25
+// x *= 2;
+// x++;
+// x--;
+// console.log(x);
 
 // comparison operator
 
-console.log(ageSagar > ageMadhu);
-console.log(ageMadhu >= 18);
+// console.log(ageSagar > ageMadhu);
+// console.log(ageMadhu >= 18);
 
-const isFullage = ageMadhu >= 18;
-console.log(isFullage);
+// const isFullage = ageMadhu >= 18;
+// console.log(isFullage);
 
-console.log(now - 2000 > now - 2007);
+// console.log(now - 2000 > now - 2007);
 
-const averageAge = (ageSagar + ageMadhu) / 2;
-console.log(ageSagar, ageMadhu, averageAge);
+// const averageAge = (ageSagar + ageMadhu) / 2;
+// console.log(ageSagar, ageMadhu, averageAge);
+
+const firstName = "Sagar";
+const job = "engineer";
+const birthYear = 2000;
+const year = 2023;
+const sagar =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(sagar);
+
+const newSagar = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`;
+console.log(newSagar);
+
+console.log(
+  "im super\n\
+and fine\n\
+perfect"
+);
+
+console.log(`I'm Sagar
+and im
+engineer`);
