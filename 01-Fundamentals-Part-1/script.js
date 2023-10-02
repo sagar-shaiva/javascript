4e0d287fbc3bb76c8608b1e649563e6f8e70f68f
sagar <s.sagar2468@gmail.com>
2023-10-02 10:49:13 +0530

    data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(1 + 22 + 24);
 
@@ -13,3 +14,24 @@ let PI = 3.1415;
 let myFirstJob = "engineer";
 let currentJob = "Product Support Engineer (Slack)";
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof "sagar");
+
+javascriptIsFun = "yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year);
+
+console.log(typeof null);
