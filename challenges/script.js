8dd1738173ddfd08122786394217d736aa2ea369
sagar <s.sagar2468@gmail.com>
2023-10-04 20:25:34 +0530

    coding exercise 5 complete

--- challenges/script.js
+++ challenges/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Challenge 1
 
 // const massMark = 78;
@@ -61,10 +62,48 @@
 
 // CHALLENGE 4 ---------------
 
-const bill = 275;
+// const bill = 275;
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-console.log(
-  `The bill was ${bill} , the tip was ${tip}, and the total value ${bill + tip}`
-);
+// console.log(
+//   `The bill was ${bill} , the tip was ${tip}, and the total value ${bill + tip}`
+// );
+
+// JavaScript Fundamentals – Part 2-------------------------------------
+
+// CODING EXERCISE 5---------------------------------
+
+const calcAverage = (score1, score2, score3) => {
+  return (score1 + score2 + score3) / 3;
+};
+
+const scoreDolphins = function (score1, score2, score3) {
+  const avg = calcAverage(score1, score2, score3);
+  return avg;
+};
+const scoreKoalas = function (score1, score2, score3) {
+  const avg = calcAverage(score1, score2, score3);
+  return avg;
+};
+
+let avgDolphins = scoreDolphins(44, 23, 71);
+let avgKoalas = scoreKoalas(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphins win`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koalas win`);
+  } else {
+    console.log("no team wins");
+  }
+};
+
+//test 2 ------
+
+avgDolphins = scoreDolphins(85, 54, 41);
+avgKoalas = scoreKoalas(23, 34, 27);
+console.log(avgDolphins, avgKoalas);
+
+checkWinner(avgDolphins, avgKoalas);
