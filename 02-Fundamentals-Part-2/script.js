7d47b967fd3cc0197a6189acc3cbe5bf3b535b65
sagar <s.sagar2468@gmail.com>
2023-10-04 18:05:24 +0530

    funtions declaration and funtion expression

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+// let hasDriversLicence = false;
+
+// const testPass = true;
+
+// if (testPass) hasDriversLicence = true;
+
+// if (hasDriversLicence) console.log("I can drive");
+
+// const interface = "audio";
+// const private = "audio";
+
+// FUNCTIONS ------------------
+
+function logger() {
+  console.log("I'm Sagar");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//FUNCTION DECLARATION
+
+const age1 = calcAge1(2000);
+
+function calcAge1(birthYear) {
+  return 2050 - birthYear;
+}
+
+// FUNCTION EXPRESSION
+
+const calcAge2 = function (birthYear) {
+  return 2050 - birthYear;
+};
+
+const age2 = calcAge2(2007);
+console.log(age1, age2);
