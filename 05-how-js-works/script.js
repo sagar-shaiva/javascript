c97cd2d292c9ca6d74958ed0ed3e880096e9c914
sagar <s.sagar2468@gmail.com>
2023-10-15 23:23:20 +0530

    scoping

--- 05-how-js-works/script.js
+++ 05-how-js-works/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age},born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'suraj';
+      output = 'New Output';
+
+      const str = `oh ,and you're a millenial,${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      //   const output = 'New Output';
+    }
+
+    console.log(millenial);
+    console.log(output);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'sagar';
+calcAge(1995);
+
+// console.log(age);
+// printAge();
