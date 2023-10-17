2f7942fefa02c5ae8626abe562ca83b789df3687
sagar <s.sagar2468@gmail.com>
2023-10-17 14:05:21 +0530

    arrays and objects destructuring

--- 06-DataStructure-modernOperators-Strings/script.js
+++ 06-DataStructure-modernOperators-Strings/script.js
@@ -1,1 +1,134 @@
 'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  //order delivery
+  orderDelivery: function ({
+    starterIndex = 0,
+    mainIndex,
+    address,
+    time = '9:00',
+  }) {
+    console.log(
+      `order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+/*
+
+restaurant.orderDelivery({
+  time: '12:00',
+  address: '22',
+  starterIndex: 2,
+  mainIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: '22',
+  mainIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//nested objects
+
+// const { fri } = openingHours;
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+*/
+
+///////////////////////////////////////////////
+//destructuring arrays
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main.secondary);
+
+// // let [d, e] = ['sagar', 'madhu'];
+// // console.log(d);
+// // console.log(e);
+// // [d, e] = [e, d];
+// // console.log(d, e);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// //nested destructuring
+// const nested = [2, 4, [5, 6]];
+
+// // const [i, , j] = nested;
+// // console.log(i, j);
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // default values
+
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
