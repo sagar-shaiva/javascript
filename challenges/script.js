707f3522ebd70d0cc0a72c4a89453a98d6aed186
sagar <S.Sagar2468@gmail.com>
2023-10-24 14:29:46 +0530

    challenge 9 complete

--- challenges/script.js
+++ challenges/script.js
@@ -202,14 +202,81 @@
 // console.log(calcAverage(totals));
 // console.log(calcAverage(tips));
 
-const arr = [17, 21, 23];
+// const arr = [17, 21, 23];
 
-const printForecast = function (arr) {
-  let str = '';
-  for (let i = 0; i < arr.length; i++) {
-    str += `${arr[i]}°C in ${i + 1}days...`;
-  }
-  console.log('...' + str);
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}°C in ${i + 1}days...`;
+//   }
+//   console.log('...' + str);
+// };
+
+// printForecast(arr);
+
+// coding exercise 9 -----------------------------------
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
-printForecast(arr);
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...player1, ...player2];
+
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goal was scored`);
+};
+
+printGoals(...game.scored);
+
+team1 < team2 && console.log('team 1 likely to win');
+team1 > team2 && console.log('team 2 likely to win');
