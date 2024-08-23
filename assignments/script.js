c0f84c5c60ef20bb89862ca2e72322bba156c117
sagar <S.Sagar2468@gmail.com>
2024-08-23 21:03:21 +0530

    assignment till basic operator

--- assignments/script.js
+++ assignments/script.js
@@ -1,8 +1,37 @@
 //values and variables
-let country = "India";
-let continent = "Asia";
-let population = "140cr";
+const country = "India";
+const continent = "Asia";
+let population = 140;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Data types
+
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+//let, const and var
+language = "Kannada";
+// isIsland = true;
+
+//Basic Operators
+population /= 2;
+population++;
+console.log(population > 6);
+console.log(population < 33);
+
+const description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " million people speaks " +
+  language;
+console.log(description);
