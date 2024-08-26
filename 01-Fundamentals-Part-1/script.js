8259b6459900d05b91d6030e7e8366bf9df2bc66
sagar <S.Sagar2468@gmail.com>
2024-08-26 22:19:29 +0530

    type conversion and coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -135,3 +135,27 @@ if (birthYear <= 2000) {
 
 console.log(century);
 */
+
+//Type Conversion
+
+const inputYear = "2000";
+console.log(Number(inputYear, inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number("jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+
+console.log("I'm" + 23 + "years old");
+console.log("I'm" + 23 + "years old");
+console.log("23" - "10" + 3);
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+console.log("23" / "2");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
