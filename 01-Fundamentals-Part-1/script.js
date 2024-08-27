036e8791e7e720004f1a545f32758caa6c062907
sagar <S.Sagar2468@gmail.com>
2024-08-27 23:01:41 +0530

    truthy and falsy values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -136,6 +136,7 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+/*
 //Type Conversion
 
 const inputYear = "2000";
@@ -159,3 +160,33 @@ console.log("23" / "2");
 let n = "1" + 1;
 n = n - 1;
 console.log(n);
+console.log("123" < 57);
+*/
+/*
+//truthy and falsy value
+
+// 5 falsy value 0,',undefined,null,NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Sagar"));
+console.log(Boolean(""));
+console.log(Boolean({}));
+
+const money = 100;
+
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("you should get a job");
+}
+
+// let height;
+let height = 5.8;
+
+if (height) {
+  console.log("height is defined");
+} else {
+  console.log("Height is undefined");
+}
+*/
