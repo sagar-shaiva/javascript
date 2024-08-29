f35571787eac758f05e0621d6271743efe14df92
sagar <S.Sagar2468@gmail.com>
2024-08-29 21:40:28 +0530

    conditional ternary operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -283,3 +283,23 @@ if (day === "monday") {
 }
 
 */
+/*
+//conditional operator(ternary)
+const age = 24;
+// age >= 18
+//   ? console.log("I like to drink Wine🍷")
+//   : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(` i like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
