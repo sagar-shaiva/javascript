8e8274a98f65142119f4a45982cfcde1a4b9eac7
sagar <S.Sagar2468@gmail.com>
2024-08-30 20:17:25 +0530

    funtions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -13,3 +13,32 @@ if (hasDriversLicense) console.log("I can drive");
 // const private = 234;
 // const if = 22;
 */
+
+//Functions
+
+function logger() {
+  console.log("Hello my name is Sagar");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apple, orange) {
+  console.log(apple, orange);
+
+  const juice = `Juice with ${apple} apples and ${orange} oranges.`;
+
+  return juice;
+}
+
+const appleJuice = fruitProcessor(3, 5);
+
+console.log(appleJuice);
+console.log(fruitProcessor(2, 3));
+
+const appleOrangeJuice = fruitProcessor(2, 6);
+
+console.log(appleOrangeJuice);
+
+const num = Number("24");
