491c5e14cf93782a3f8c50c24b95fb202d438a91
sagar <S.Sagar2468@gmail.com>
2024-08-30 17:20:53 +0530

    strict mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+//activating strict mode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest === true) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio";
+// const private = 234;
+// const if = 22;
+*/
