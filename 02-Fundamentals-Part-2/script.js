49691a408f0b2dfcc19ca8fdb0219b601bec8468
sagar <S.Sagar2468@gmail.com>
2024-09-01 18:25:42 +0530

    function declarations and expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,6 +14,7 @@ if (hasDriversLicense) console.log("I can drive");
 // const if = 22;
 */
 
+/*
 //Functions
 
 function logger() {
@@ -42,3 +43,22 @@ const appleOrangeJuice = fruitProcessor(2, 6);
 console.log(appleOrangeJuice);
 
 const num = Number("24");
+
+*/
+
+//Function declaration
+function calcAge1(birthYear) {
+  return 2024 - birthYear;
+}
+
+const age1 = calcAge1(2000);
+console.log(age1);
+
+// Fucntion expressions
+
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const age2 = calcAge2(2000);
+console.log(age1, age2);
