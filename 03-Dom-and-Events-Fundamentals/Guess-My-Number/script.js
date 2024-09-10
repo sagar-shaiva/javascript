480caaa3f8c94b16ddb83bfd8ae702cfd335fd0e
sagar <S.Sagar2468@gmail.com>
2024-09-10 23:09:50 +0530

    manipulating css styles

--- 03-Dom-and-Events-Fundamentals/Guess-My-Number/script.js
+++ 03-Dom-and-Events-Fundamentals/Guess-My-Number/script.js
@@ -17,10 +17,18 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+
+  // when no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+
+    //if player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too high!';
@@ -29,6 +37,7 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game';
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉Too low!';
