2e3497d316eee863f991360241ed49b8f1d9358f
sagar <S.Sagar2468@gmail.com>
2024-09-13 23:45:28 +0530

    working with classes

--- 03-Dom-and-Events-Fundamentals/Modal-Window/script.js
+++ 03-Dom-and-Events-Fundamentals/Modal-Window/script.js
@@ -1,1 +1,24 @@
-"use strict";
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
