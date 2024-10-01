d4862e8de64096f04cff25ce2f0bae9fe960d6b1
sagar <s.sagar2468@gmail.com>
2024-10-02 00:33:55 +0530

    resetting the game

--- 03-Dom-and-Events-Fundamentals/Pig-Game/script.js
+++ 03-Dom-and-Events-Fundamentals/Pig-Game/script.js
@@ -11,14 +11,24 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -57,9 +67,9 @@ btnHold.addEventListener('click', () => {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    //2. check if player score >=100
+    //2. check if player score >=50
 
-    if (scores[activePlayer] >= 21) {
+    if (scores[activePlayer] >= 50) {
       playing = false;
       diceEl.classList.add('hidden');
 
@@ -76,3 +86,5 @@ btnHold.addEventListener('click', () => {
 
   //3.switch to the next player
 });
+
+btnNew.addEventListener('click', init);
