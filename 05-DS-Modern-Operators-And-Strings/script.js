e55466da879057faef4f9d9848595bc2d40c9d88
sagar <s.sagar2468@gmail.com>
2024-10-08 23:39:43 +0530

    destructuring arrays

--- 05-DS-Modern-Operators-And-Strings/script.js
+++ 05-DS-Modern-Operators-And-Strings/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +30,29 @@ const restaurant = {
     },
   },
 };
+
+//destructuring arrays
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(1, 2);
+console.log(starter, mainCourse);
+
+//nested destructuring
+const nested = [1, 3, [4, 5]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
