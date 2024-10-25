1e95fe1a5884cb1bd20639c317a35c129cf63c2f
sagar <s.sagar2468@gmail.com>
2024-10-25 23:13:40 +0530

    maps iteration

--- 05-DS-Modern-Operators-And-Strings/script.js
+++ 05-DS-Modern-Operators-And-Strings/script.js
@@ -47,7 +47,42 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+//maps iteration
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'c'],
+  [2, 'java'],
+  [3, 'javascript'],
+  ['correct', 3],
+  [true, 'correct🎉'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const answer = Number(prompt('Your Answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+//convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
 //Maps
+/*
 const rest = new Map();
 
 rest.set('name', 'sagar');
@@ -77,6 +112,7 @@ rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
 console.log(rest.size);
 console.log(rest.get(arr));
+*/
 //SETS
 /*
 const orderSet = new Set(['Pasta', 'Pizza', 'Risotto', 'Pizza', 'Pasta']);
