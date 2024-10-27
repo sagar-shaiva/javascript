272cdd84728f0e2681e98bc8cc97b6836b244141
sagar <s.sagar2468@gmail.com>
2024-10-27 13:53:19 +0530

    strings method practice

--- 05-DS-Modern-Operators-And-Strings/script.js
+++ 05-DS-Modern-Operators-And-Strings/script.js
@@ -47,7 +47,25 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-
+//string method practice
+/*
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+  console.log(output);
+}
+*/
 //working with strings part-3
 /*
 //split
