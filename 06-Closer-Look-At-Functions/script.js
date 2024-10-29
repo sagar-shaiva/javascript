358fe10221d3295431c2a739e80df45209186bc1
sagar <s.sagar2468@gmail.com>
2024-10-29 21:23:56 +0530

    function accepting callback functions

--- 06-Closer-Look-At-Functions/script.js
+++ 06-Closer-Look-At-Functions/script.js
@@ -23,6 +23,7 @@ createBooking('LH123', undefined, 5);
 */
 
 //passsing arguments primitive vs referece
+/*
 const flight = 'LM234';
 const sagar = {
   name: 'Sagar',
@@ -49,3 +50,31 @@ const newPassport = function (person) {
 
 newPassport(sagar);
 checkIn(flight, sagar);
+*/
+
+//Function accepting callback functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher Order Function
+const transformer = function (str, fn) {
+  console.log(`Original String:${str} `);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name} `);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+//js uses callback all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['sagar', 'suraj', 'madhu'].forEach(high5);
