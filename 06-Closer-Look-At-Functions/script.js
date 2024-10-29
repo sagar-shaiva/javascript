fe121d2bbcc45601ba8f7ec4b1fd3c764cb3fc3a
sagar <s.sagar2468@gmail.com>
2024-10-29 14:15:40 +0530

    passsing arguments primitive vs referece

--- 06-Closer-Look-At-Functions/script.js
+++ 06-Closer-Look-At-Functions/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //deafault parameters
+/*
 const bookings = [];
 const createBooking = function (
   flightNum,
@@ -19,3 +20,32 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 5);
+*/
+
+//passsing arguments primitive vs referece
+const flight = 'LM234';
+const sagar = {
+  name: 'Sagar',
+  passport: 24813746372,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'S ' + passenger.name;
+  if (passenger.passport === 24813746372) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+// checkIn(flight, sagar);
+// console.log(flight);
+// console.log(sagar);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(sagar);
+checkIn(flight, sagar);
