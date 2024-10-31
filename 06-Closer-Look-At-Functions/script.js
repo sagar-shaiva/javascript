7178ac26f9a0325b9528e1b58448f9a1f6e6c9d9
sagar <s.sagar2468@gmail.com>
2024-10-31 20:13:50 +0530

    more examples on closers

--- 06-Closer-Look-At-Functions/script.js
+++ 06-Closer-Look-At-Functions/script.js
@@ -285,7 +285,7 @@ console.log(notPrivate);
 */
 
 //Closers
-
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -302,3 +302,43 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+//more closers examples
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+//reassigned f function
+h();
+f();
+console.dir(f);
+//example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(180, 3);
