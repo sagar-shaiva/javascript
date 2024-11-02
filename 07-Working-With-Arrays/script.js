05fdbf29df013925dc0942db8c2b40b7b3a4f13e
sagar <s.sagar2468@gmail.com>
2024-11-02 16:10:49 +0530

    New at Method

--- 07-Working-With-Arrays/script.js
+++ 07-Working-With-Arrays/script.js
@@ -74,7 +74,17 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//New at Method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log('sagar'.at(0));
+console.log('sagar'.at(-1));
 //Simple Array Methods
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 //slice Method
@@ -108,3 +118,4 @@ console.log([...arr, ...arr2]);
 
 //join
 console.log(letters.join('-'));
+*/
