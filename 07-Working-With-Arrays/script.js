1ab7f257c9e33f2e4e7a55361d9f2b175030acad
sagar <s.sagar2468@gmail.com>
2024-11-02 14:48:28 +0530

    simple array methods

--- 07-Working-With-Arrays/script.js
+++ 07-Working-With-Arrays/script.js
@@ -74,3 +74,37 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//Simple Array Methods
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//slice Method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+//Splice (Mutates Array)
+
+// console.log(arr.splice(2)); //deletes all elements
+arr.splice(-1); //deletes last element
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//Reverse (mutates arrays)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'j'];
+
+console.log(arr2.reverse()); //reverse method mutates arrays
+console.log(arr2);
+
+//concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//join
+console.log(letters.join('-'));
