bca5399c8110398bc7fab2c1e65654a67dc1d3e5
sagar <s.sagar2468@gmail.com>
2024-12-24 12:53:59 +0530

    prototypes

--- 10-OOP-With-Javascript/script.js
+++ 10-OOP-With-Javascript/script.js
@@ -8,3 +8,18 @@ const Person = function (firstName, birthYear) {
 const sagar = new Person('sagar', 2000);
 console.log(sagar);
 console.log(sagar instanceof Person);
+
+//Prototypes (only one copy)
+
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.birthYear);
+};
+
+sagar.calcAge(2000);
+console.log(Person.prototype.isPrototypeOf(sagar));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(sagar.species);
+
+console.log(sagar.hasOwnProperty('species'));
+console.log(sagar.hasOwnProperty('firstName'));
