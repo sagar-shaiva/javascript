362ce961d7c2da5ed31cdcb5e805a2ef8aab7264
sagar <s.sagar2468@gmail.com>
2024-12-24 12:46:42 +0530

    constructor funcion and new operator

--- 10-OOP-With-Javascript/script.js
+++ 10-OOP-With-Javascript/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const sagar = new Person('sagar', 2000);
+console.log(sagar);
+console.log(sagar instanceof Person);
