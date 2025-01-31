3a6c860fa0edc43f1f5553429e81f2a2e0d0c74f
S Sagar <s.sagar2468@gmail.com>
2025-01-31 23:34:30 +0530

    challenge 2

--- assignments/script.js
+++ assignments/script.js
@@ -1331,7 +1331,7 @@ Test data:
 § Data 1: [5, 2, 4, 1, 15, 8, 3]
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 */
-
+/*
 const calcAverageHumanAge = function(ages){
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);  
@@ -1343,5 +1343,21 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 
+*/
 
+//challenege 3
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
 
+*/
+
+const calcAverageHumanAge = ages=>ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4)).filter(age=> age>=18).reduce((acc,age,_,arr) =>acc+age/arr.length,0 );
+  
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
