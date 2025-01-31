9d5429b022ac1824e738067dc3cf24199b751eac
S Sagar <ss40094959@wipro.com>
2025-01-31 14:09:44 +0530

    a magic of chaining methods

--- 07-Working-With-Arrays/script.js
+++ 07-Working-With-Arrays/script.js
@@ -99,6 +99,20 @@ const calcDisplayBalance = function (movements) {
 };
 calcDisplayBalance(account1.movements);
 
+const EurToUsd =1.1;
+const calcDisplaySummary = function (movements) { 
+const incomes = movements.filter(mov=> mov>0).reduce((acc,mov)=>acc+mov,0);
+labelSumIn.textContent = incomes;
+const out = movements.filter(mov=> mov<0).reduce((acc,mov)=>acc+mov,0);
+labelSumOut.textContent = `${Math.abs(out)}`;
+const interest = movements.filter(mov=> mov>0).map(deposit=>(deposit*1.2)/100).filter(int=> int>=1).reduce((acc,int)=>acc+int,0);
+labelSumInterest.textContent = interest;
+
+
+}
+
+calcDisplaySummary(account1.movements);
+
 //max values
 /*
 const max = movements.reduce((acc, mov) => {
@@ -243,3 +257,8 @@ currenciesUnique.forEach(function (value, _, map) {
 */
 
 //Magic of Chaining Methods
+
+const EurToUsds = 1.1;
+
+const  totalDepositsUsd = movements.filter(mov=> mov>0).map(mov=> mov*EurToUsds).reduce((acc,mov)=>acc+mov,0);
+console.log(totalDepositsUsd);
