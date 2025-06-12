475c1e4e6f3a1fbe5383c94ea078513c7140a770
S Sagar <s.sagar2468@gmail.com>
2025-06-12 21:12:18 +0530

    the remainder operator

--- 08-Numbers-Dates-Intl-Timers/script.js
+++ 08-Numbers-Dates-Intl-Timers/script.js
@@ -287,7 +287,7 @@ console.log(Number.isInteger(23/0));
 */
 
 //math and rounding 
-
+/*
 console.log(Math.sqrt(25));
 console.log(25** (1/2));
 console.log(8**(1/3));
@@ -331,3 +331,27 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.744).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+*/
+
+//The remainder operator
+
+console.log(5%2);
+console.log(5/2);
+
+console.log(8%3);
+console.log(8/3);
+
+const isEven= n=>n%2===0;
+console.log(isEven(2));
+console.log(isEven(23));
+console.log(isEven(2468));
+
+labelBalance.addEventListener('click',function(){[...document.querySelectorAll('.movements__row')].forEach(function(row,i){
+ //2 4 6 8
+  if(i%2===0) row.style.backgroundColor='orangred';
+//0 3 6 9
+  if(i%3===0) row.style.backgroundColor='blue';
+})
+
+});
