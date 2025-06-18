fe080503421a95f2b719c8ae594a097a1bf578d9
S Sagar <s.sagar2468@gmail.com>
2025-06-19 00:03:17 +0530

    consuming promises

--- 12-Asynchronous-Javascript/script.js
+++ 12-Asynchronous-Javascript/script.js
@@ -10,7 +10,22 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
-
+//our first ajax call:XMLHttpRequest
+const renderCountry=function(data,className =''){
+     const html= `  <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`;
+        countriesContainer.insertAdjacentHTML('beforeend',html);
+        countriesContainer.style.opacity = 1;
+};
+/*
 const getCountryAndNeighbour= function(country){
 const request = new XMLHttpRequest();
 
@@ -59,3 +74,13 @@ request2.addEventListener('load',function(){
 })
 };
 getCountryAndNeighbour('usa');
+*/
+
+const request = fetch("https://restcountries.com/v2/name/portugal");
+console.log(request);
+
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v2/name/${country}`).then(response=> response.json()).then(data=>renderCountry(data[0]));
+};
+
+    getCountryData('portugal')
