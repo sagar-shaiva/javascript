d028e8e9448d8d3dfa71a9d17ae14ddf2955e79e
S Sagar <s.sagar2468@gmail.com>
2025-06-19 21:20:15 +0530

    chaining promises

--- 12-Asynchronous-Javascript/script.js
+++ 12-Asynchronous-Javascript/script.js
@@ -80,7 +80,17 @@ const request = fetch("https://restcountries.com/v2/name/portugal");
 console.log(request);
 
 const getCountryData = function(country){
-    fetch(`https://restcountries.com/v2/name/${country}`).then(response=> response.json()).then(data=>renderCountry(data[0]));
+
+  //country 1
+    fetch(`https://restcountries.com/v2/name/${country}`).then(response=> response.json()).then(data=>{
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if(!neighbour) return;
+      //country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    
+    }).then(response =>response.json()).then(data=>renderCountry(data,'neighbour'));
 };
 
     getCountryData('portugal')
