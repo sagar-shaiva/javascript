1e63d582c8a69756cda7d5799c6a7ada94c930e4
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-23 22:42:12 +0530

    Event Propagation in Practice

--- 13-Advance-DOM-and-Events/script.js
+++ 13-Advance-DOM-and-Events/script.js
@@ -133,6 +133,7 @@ logo.classList.contains('C'); //not includes
 // logo.className = 'sagar';
 */
 
+/*
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -172,3 +173,32 @@ const alertH1 = function(e){
 h1.addEventListener('mouseenter',alertH1);
 setTimeout(() => h1.removeEventListener('mouseenter',alertH1),3000);
 
+*/
+
+//event propagaton in practice
+/*
+//rgb(255,255,255)
+const randomInt = (min,max)=> Math.floor(Math.random() * (max-min)+1+min);
+const randomColor = ()=> `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+document.querySelector('.nav__link').addEventListener('click',function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('Link',e.target,e.currentTarget);
+  console.log(e.currentTarget===this);
+
+  //stop propagation
+
+  // e.stopPropagation();
+})
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('Container',e.target,e.currentTarget);
+
+})
+document.querySelector('.nav').addEventListener('click',function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('Nav',e.target,e.currentTarget);
+
+})
+
+*/
