24912a4a74ab0da35287a45b7b93cd018ca7c6c9
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-23 20:19:30 +0530

    Types of Events and Event Handlers

--- 13-Advance-DOM-and-Events/script.js
+++ 13-Advance-DOM-and-Events/script.js
@@ -151,5 +151,24 @@ btnScrollTo.addEventListener('click',function(e){
   //   s1coords.top+window.pageYOffset
   // )
 
-  section1.scrollIntoView({behavior:"smooth"})
+  section1.scrollIntoView({behavior:"smooth"});
 })
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function(e){
+  alert("addEventListener: Great! you are reading the heading :)");
+}
+
+// h1.addEventListener('mouseenter',function(e){
+//   alert("addEventListener: Great! you are reading the heading :)")
+// })
+
+// h1.onmouseenter = function(e){
+//   alert("onmouseenter: Great! you are reading the heading :)")
+
+// }
+
+h1.addEventListener('mouseenter',alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter',alertH1),3000);
+
