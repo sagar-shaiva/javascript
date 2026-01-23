76c1169440901358abdd0a65381aa58203c73925
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-23 13:52:04 +0530

    Implementing Smooth Scrolling

--- 13-Advance-DOM-and-Events/script.js
+++ 13-Advance-DOM-and-Events/script.js
@@ -132,3 +132,24 @@ logo.classList.contains('C'); //not includes
 
 // logo.className = 'sagar';
 */
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function(e){
+
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (x/y)',window.pageXOffset,window.pageYOffset);
+
+  console.log('Height/Width viewport',document.documentElement.clientHeight,document.documentElement.clientWidth);
+
+  // window.scrollTo(
+  //   s1coords.left+window.pageXOffset,
+  //   s1coords.top+window.pageYOffset
+  // )
+
+  section1.scrollIntoView({behavior:"smooth"})
+})
