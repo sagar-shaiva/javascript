9e9b76fda929ea3b02ca45cd1d0c0260367bd612
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-24 14:02:05 +0530

    Building a Tabbed Component

--- 13-Advance-DOM-and-Events/script.js
+++ 13-Advance-DOM-and-Events/script.js
@@ -84,8 +84,36 @@ document.querySelector('.nav__links').addEventListener('click',function(e){
   }
 })
 
+// Tabbed Comnponent
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click',function(e){
+  const clicked = e.target.closest('.operations__tab');
+  // console.log(clicked);
+
+  //Gaurd clause
+
+  if(!clicked) return;
+
+//remove active classes
+  tabs.forEach(t=> t.classList.remove('operations__tab--active'));
+tabsContent.forEach(c=>c.classList.remove('operations__content--active'));
+
+//active tab
+  clicked.classList.add('operations__tab--active');
 
 
+  //Active content area
+
+  // console.log(clicked.dataset.tab);
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+
+})
 ////////////////////////////////////
 /*
 //Selecting elements
