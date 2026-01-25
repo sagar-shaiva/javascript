2f69c8889b9c1e9603422a08d8ce081da333f10a
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-25 09:26:00 +0530

    Lazy Loading Images

--- 13-Advance-DOM-and-Events/script.js
+++ 13-Advance-DOM-and-Events/script.js
@@ -171,7 +171,6 @@ console.log(navHeight);
 
 const stickyNav= function(entries){
   const [entry] = entries;
-  console.log(entry);
 
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -203,6 +202,27 @@ allSections.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 })
+
+///////////////////////////////////////////////////
+//lazy loading img
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function(entries,observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+
+  //replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load',function(){
+  entry.target.classList.remove('lazy-img');
+  })
+  observer.unobserve(entry.target);
+
+}
+
+const imgObserver = new IntersectionObserver(loadImg,{rroot:null,threshold:0,rootMargin:'+200px'});
+
+imgTargets.forEach(img=> imgObserver.observe(img));
+
 
 
 /*
