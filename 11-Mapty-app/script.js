90e26d5390401b2e503c429eaac10deffccc0e6f
sagar-shaiva <s.sagar2468@gmail.com>
2026-01-31 12:17:36 +0530

    Rendering Workout Input Form

--- 11-Mapty-app/script.js
+++ 11-Mapty-app/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map,mapEvent;
+
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
@@ -18,13 +20,35 @@ if(navigator.geolocation){
         const {longitude} = position.coords;
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
         const coords = [latitude,longitude];
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-map.on('click',function(mapEvent){
+//Handling clicks on map
+map.on('click',function(mapE){
+
+    mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+
+        });
+    },
+    function(){
+        alert("Could not get your position");
+    })
+}
+
+//rendering workout from map
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    //clear input fields
+    inputDistance.value = inputDuration.value= inputCadence.value =inputElevation.value='';
+    // display the marker
     console.log(mapEvent);
     const {lat,lng}= mapEvent.latlng;
 
@@ -32,15 +56,16 @@ map.on('click',function(mapEvent){
         L.popup({
             maxWidth:250,
             minWidth:100,
-            autoCLose:false,
+            autoClose:false,
             closeOnClick:false,
             className:'running-popup',
         })
     )
     .setPopupContent('Workout')
     .openPopup();
-        });
-    },function(){
-        alert("Could not get your position");
-    })
-}
+})
+
+inputType.addEventListener('change',function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
